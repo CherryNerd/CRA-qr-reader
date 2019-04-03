19e092c091d52647a113eb14e2b4f41d8e9338a6
Renzo Roso <renzo@geex.company>
2019-04-03 13:48:36 +0200

    Ejected

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import QrReader from 'react-qr-reader'
 
 class App extends Component {
+
+  state = {
+    qrContent: ''
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+    	  <QrReader
+	      delay={300}
+              facingMode={'environment'}
+              style={{width: '30%'}}
+              onScan={data => this.setState({qrContent: data})}
+          />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
